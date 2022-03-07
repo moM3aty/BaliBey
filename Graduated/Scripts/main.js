7ce59fb04a68399942cbc441f799622500306bad
moM3aty <mo.m3aty@gmail.com>
2022-03-07 12:25:30 +0200

    Edit JS

--- Graduated/Scripts/main.js
+++ Graduated/Scripts/main.js
@@ -4,10 +4,10 @@ function myFunction() {
 
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('m3aty')) {
                 openDropdown.classList.remove('m3aty');
             }
